e62d833fed220b92e87e300fad2df0dc86120d25
Alexander Edge <alex@alexedge.co.uk>
2015-11-24 20:22:41 +0000

    Add framework target

---
+++ LogglyLogger-CocoaLumberjack/LogglyLogger-CocoaLumberjack.h
@@ -0,0 +1,21 @@
+//
+//  LogglyLogger-CocoaLumberjack.h
+//  LogglyLogger-CocoaLumberjack
+//
+//  Created by Alexander Edge on 24/11/2015.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LogglyLogger-CocoaLumberjack.
+FOUNDATION_EXPORT double LogglyLogger_CocoaLumberjackVersionNumber;
+
+//! Project version string for LogglyLogger-CocoaLumberjack.
+FOUNDATION_EXPORT const unsigned char LogglyLogger_CocoaLumberjackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+#import <LogglyLogger_CocoaLumberjack/LogglyLogger.h>
+#import <LogglyLogger_CocoaLumberjack/LogglyFormatter.h>
+#import <LogglyLogger_CocoaLumberjack/LogglyFields.h>
